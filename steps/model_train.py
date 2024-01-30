71710370460440663428c1d2d3e6c38275d73b8a
manan-bedi2908 <mananbedilps@gmail.com>
2024-01-30 16:14:33 +0530

    Model Training Pipeline completed

--- steps/model_train.py
+++ steps/model_train.py
@@ -1,13 +1,36 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
 
 @step
-def train_model(df: pd.DataFrame) -> None:
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.Series,
+    y_test: pd.Series,
+    config: ModelNameConfig
+) -> RegressorMixin:
     """
     Trains the model on the Ingested Data
 
     Args: 
-        df: The Ingested Data
+        X_train: Training Data
+        X_test: Test Data
+        y_train: Train Labels
+        y_test: Test Labels
     """
-    pass
+    try:
+        model = None
+        if config.model_name == "Linear Regression":
+            model = LinearRegressionModel()
+            trained_model = model.train(X_train, y_train)
+            return trained_model
+        else:
+            raise ValueError(f"Model {config.model_name} not supported!")
+    except Exception as e:
+        logging.error(f"Error in Training Model : {e}")
+        raise e
+    
