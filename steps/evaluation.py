9df7cb1ad7868b29501bd10f047829893b49608d
manan-bedi2908 <mananbedilps@gmail.com>
2024-01-30 19:57:12 +0530

    add mlflow experiment tracking

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -5,8 +5,12 @@ from src.evaluation import MSE, RMSE, R2
 from sklearn.base import RegressorMixin
 from typing import Tuple
 from typing_extensions import Annotated
+import mlflow
+from zenml.client import Client
 
-@step
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(
     model: RegressorMixin,
     X_test: pd.DataFrame,
@@ -27,12 +31,15 @@ def evaluate_model(
 
         mse_class = MSE()
         mse = mse_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("mse", mse)
 
         r2_class = R2()
         r2 = r2_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("r2", r2)
 
         rmse_class = RMSE()
         rmse = rmse_class.calculate_scores(y_test, prediction)
+        mlflow.log_metric("rmse", rmse)
 
         return r2, rmse
     
