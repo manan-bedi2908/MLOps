2f87f69bc8959eb85622f3703c87126eab343831
manan-bedi2908 <mananbedilps@gmail.com>
2024-01-30 16:03:14 +0530

    Add linear regression model

--- src/model_dev.py
+++ src/model_dev.py
@@ -1,5 +1,6 @@
 import logging
 from abc import ABC, abstractmethod
+from sklearn.linear_model import LinearRegression
 
 class Model(ABC):
     """
@@ -16,3 +17,26 @@ class Model(ABC):
             None
         """
         pass
+
+class LinearRegressionModel(Model):
+    """
+    Linear Regression Model
+    """
+    def train(self, X_train, y_train, **kwargs):
+        """
+        Trains the Model
+        Args:
+            X_train: Training Data
+            y_train: Train Labels
+        Returns: 
+            None
+        """
+        try:
+            reg = LinearRegression(**kwargs)
+            reg.fit(X_train, y_train)
+            logging.info("Model Training Completed...")
+            return reg
+        except Exception as e:
+            logging.error(f"Error in Training Model: {e}")
+            raise e
+
