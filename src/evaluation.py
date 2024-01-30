faa027f3dff211dbd6e369116474e83d60312823
manan-bedi2908 <mananbedilps@gmail.com>
2024-01-30 16:20:05 +0530

    MSE Evaluation Strategy added

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,6 +1,7 @@
 import logging
 from abc import ABC, abstractmethod
 import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score
 
 class Evaluation(ABC):
     """
@@ -17,3 +18,18 @@ class Evaluation(ABC):
             None
         """
         pass
+
+class MSE(Evaluation):
+    """
+    Evaluation Strategy that uses Mean Squared Error
+    """
+    def calculate_scores(self, y_true: np.ndarray, y_pred: np.ndarray):
+        try:
+            logging.info("Calculating MSE...")
+            mse = mean_squared_error(y_true, y_pred)
+            logging.info(f"MSE: {mse}")
+            return mse
+        except Exception as e:
+            logging.error(f"Error in Calculating MSE: {e}")
+            raise e
+        
